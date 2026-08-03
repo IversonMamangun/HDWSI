566aea83865d972379756b7393d771094bb4204d
IversonMamangun <177076443+IversonMamangun@users.noreply.github.com>
2026-08-03 14:51:27 +0800

    feat: create LearnPath component for showcasing educational offerings

---
+++ resources/js/components/landing/LearnPath.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+    <div class="max-w-7xl mx-auto bg-white">
+        <img src="assets/front-page/IMAGE/Learning Pathways.png" alt="">
+        <h1>Learning Pathways</h1>
+        <h2>HDWSI Offer education through multiple formats:</h2>
+    </div>
+    
+  </section>
+</template>
