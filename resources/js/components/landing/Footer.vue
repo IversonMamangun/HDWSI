ad5c64c84c6453277ccd47b2c8d6c67e5b317a72
IversonMamangun <177076443+IversonMamangun@users.noreply.github.com>
2026-08-04 23:36:45 +0800

    feat: add Footer component with initial structure

---
+++ resources/js/components/landing/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="w-full bg-gradient-to-r from-[#104C7D] via-[#2095E0] to-[#104C7D] py-20 text-white overflow-hidden">
+
+    
+  </section>
+</template>
