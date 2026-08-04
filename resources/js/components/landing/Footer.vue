0268aaeb0a985f7b38ae3687cb27cb49ee37e682
IversonMamangun <177076443+IversonMamangun@users.noreply.github.com>
2026-08-05 00:39:54 +0800

    feat: update Footer component with current year and design credits

--- resources/js/components/landing/Footer.vue
+++ resources/js/components/landing/Footer.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+
+
+const currentYear = computed(() => new Date().getFullYear());
+
 </script>
 
 <template>
-  <section class="w-full bg-gradient-to-r from-[#104C7D] via-[#2095E0] to-[#104C7D] py-20 text-white overflow-hidden">
-
-    
-  </section>
+  <footer class="w-full bg-[#2095E0] p-5 text-white overflow-hidden border-t-5 border-white">
+     <h2>
+      © {{ currentYear }} HUMAN-CENTERED DIGITAL WELLNESS AND SOCIAL INTELLIGENCE. All Rights Reserved.
+     </h2>
+     <div>
+                Designed & Developed by 
+                <a 
+                    class="hover:underline font-semibold" 
+                    href="https://bb88advertising.com/" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    BB 88 Advertising and Digital Solutions Inc.
+                </a>
+            </div>
+  </footer>
 </template>
