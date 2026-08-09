4c5b3c13e1ee7f0d1b80ba66c903b235431ad15e
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-08-09 23:49:23 +0800

    feat(model): update User model to include additional fields and full name accessor

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration {
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone_number')->nullable();
             $table->timestamp('phone_number_verified_at')->nullable();
+            $table->string('address')->nullable();
             $table->date('date_of_birth')->nullable();
             $table->string('id_type')->nullable();
             $table->string('id_number')->nullable();
