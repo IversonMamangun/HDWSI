1d6100192d626307a3bb499b76ef9a3ed88c6ae4
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-08-10 17:19:37 +0800

    feat: define User type and update auth and index exports

--- resources/js/types/auth.ts
+++ resources/js/types/auth.ts
@@ -1,40 +1,4 @@
-export type User = {
-    id: number;
-
-    first_name: string;
-    middle_name: string | null;
-    last_name: string;
-
-    full_name: string;
-    initials: string;
-
-    avatar?: string;
-
-    email: string;
-    email_verified: boolean;
-    email_verified_at: string | null;
-
-    phone_number: string | null;
-    phone_number_verified_at: string | null;
-
-    date_of_birth: string | null;
-    address: string | null;
-
-    id_type: string | null;
-    id_number: string | null;
-
-    roles: string[];
-    primary_role: string | null;
-
-    created_at: string;
-    updated_at: string;
-
-    can: {
-        view: boolean;
-        update: boolean;
-        delete: boolean;
-    };
-};
+import  type { User } from "./user";
 
 export type Auth = {
     user: User;
