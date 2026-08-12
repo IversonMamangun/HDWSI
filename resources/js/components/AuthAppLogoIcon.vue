20514f5f443197832841c66de880d895f9d79373
Ahllaine Christian De Ocera <acdeocera.dev@gmail.com>
2026-08-12 14:30:22 +0800

    feat(auth): integrate AuthAppLogoIcon component and update layout for improved branding

---
+++ resources/js/components/AuthAppLogoIcon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+
+defineOptions({
+    inheritAttrs: false,
+});
+
+type Props = {
+    className?: HTMLAttributes['class'];
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+    <img
+        src="/assets/registration/registration_logo.png"
+        alt="HDWSI"
+        :class="className"
+        class="mx-auto h-full w-auto max-w-full object-contain"
+        v-bind="$attrs"
+    />
+</template>
